d0ccef1169ab77441830e22c8fa75fd875867243
Miłosz <milosz.devmail@gmail.com>
2023-01-19 00:09:36 +0100

    Set initial tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"yellow-primary": '#FFD121',
+				"black-primary": '#201E1E'
+			}
+		},
+		fontFamily: {
+			Odibee: ["'Odibee Sans', cursive"]
+		},
+		screens: {
+			tablet: '768px',
+			desktop: '1024px',
+			bigDesktop: '1280px',
+			huge: '1440px'
+		}
 	},
 	plugins: [],
 }
